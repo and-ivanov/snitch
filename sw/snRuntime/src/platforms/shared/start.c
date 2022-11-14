5723a19acdf77a8e9457b8510b5429657a33dca9
Andrei Ivanov <9387186+and-ivanov@users.noreply.github.com>
2022-11-14 16:45:01 +0100

    Increase stack size 1 KB -> 8 MB

--- sw/snRuntime/src/platforms/shared/start.c
+++ sw/snRuntime/src/platforms/shared/start.c
@@ -11,7 +11,7 @@ extern const uint32_t _snrt_cluster_cluster_num;
 extern const uint32_t _snrt_cluster_cluster_id;
 void *const _snrt_cluster_global_offset = (void *)0x10000000;
 
-const uint32_t snrt_stack_size __attribute__((weak, section(".rodata"))) = 10;
+const uint32_t snrt_stack_size __attribute__((weak, section(".rodata"))) = 23;  // 8 MB per thread, as power of two
 
 // The boot data generated along with the system RTL.
 // See `ip/test/src/tb_lib.hh` for details.
